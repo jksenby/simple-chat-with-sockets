00771944a864ad2324c54c8535bcaf1d3b008910
The Best <zhalgas.karsenbai@gmail.com>
2024-01-15 22:17:24 +0600

    finished

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const socket = io();
 
 const form = document.querySelector("form"),
   message = document.querySelector("#messageInput"),
-  submitButton = document.querySelector("#submitButton");
+  submitButton = document.querySelector("#submitButton"),
+  messages = document.querySelector("#messages");
 
 const scrollToBottom = () => {
   const chatContainer = $("#chat-container");
@@ -37,56 +38,103 @@ form.addEventListener("submit", (e) => {
   }
 });
 
-function createMessage(user, message) {
+function createMessage(user, message, hour, minute) {
   const isAuthor = socket.id === user.socketId;
-  const flex = isAuthor ? "justify-content-end" : "justify-content-start";
-  const color = isAuthor ? "sent" : "received";
-  return /*html*/ `
-        <li class="d-flex ${flex} w-100">
-          <div class="flex-column ${color}" id="message">
-            <h5>
+  const random = Math.floor(Math.random() * (7 - 1) + 1);
+  let usernameColor;
+  minute = +minute < 10 ? "0" + minute : minute;
+
+  switch (random) {
+    case 1:
+      usernameColor = "#FF0000";
+      break;
+    case 2:
+      usernameColor = "#0000FF";
+      break;
+    case 3:
+      usernameColor = "#800080";
+      break;
+    case 4:
+      usernameColor = "#FFA500";
+      break;
+    case 5:
+      usernameColor = "#FFC0CB";
+      break;
+    case 6:
+      usernameColor = "#FFFF00";
+      break;
+    case 7:
+      usernameColor = "#00FFFF";
+      break;
+    default:
+      usernameColor = "#000000";
+  }
+  return !isAuthor
+    ? /*html*/ `
+        <li class="d-flex justify-content-start w-100">
+          <div class="flex-column received" id="message">
+          <div class="d-flex w-100 align-items-center justify-content-between">
+            <h5 style="color: ${usernameColor}">
               ${user.username}
             </h5>
+            <small>${hour}:${minute}</small>
+            </div>
+            <div class="text-message">
               ${message}
+            </div>
           </div>
         </li>
-        `;
+        `
+    : /*html*/ `<li class="d-flex justify-content-end w-100">
+    <div class="flex-column sent" id="message">
+    <div class="d-flex w-100 justify-content-between">
+    <div class="text-message">
+        ${message}
+      </div>
+      <small>${hour}:${minute}</small>
+      </div>
+      
+    </div>
+  </li>`;
 }
 socket.on("chat message", function (data) {
-  const { user, msg } = data;
-  $("#messages").append(createMessage(user, msg));
+  const { user, message, hour, minute } = data;
+  $("#messages").append(createMessage(user, message, hour, minute));
   scrollToBottom();
 });
 
-function createCard(username, userid, time) {
-  return `
+function createCard(username, time) {
+  return /*html*/ `
   <a href="#" class="list-group-item list-group-item-action py-3 lh-sm">
       <div class="d-flex w-100 align-items-center justify-content-between">
         <strong class="mb-1">${username}</strong>
         <small class="text-body-secondary">${time}</small>
       </div>
-      <div class="col-10 mb-1 small">${userid}</div>
     </a>
   `;
 }
 socket.on("update users", function (users) {
   $("#user-list").empty();
   users.forEach((u) => {
-    $("#user-list").append($(createCard(u.username, u.socketId, u.time)));
+    $("#user-list").append($(createCard(u.username, u.time)));
   });
 });
 
 socket.on("user joined", function (user) {
-  $("#messages").append(
-    $('<li class="system-message">').text(`${user.username} joined the chat`)
-  );
+  $(
+    "#messages"
+  ).append(/*html*/ `<li class="d-flex justify-content-center w-100 system-message">
+ ${user.username} joined the chat
+</li>`);
   scrollToBottom();
 });
 
 socket.on("user left", function (user) {
-  $("#messages").append(
-    $('<li class="system-message">').text(`${user.username} left the chat`)
-  );
+  $(
+    "#messages"
+  ).append(/*html*/ `<li class="d-flex justify-content-center w-100 system-message">
+ ${user.username} left the chat
+</li>`);
   scrollToBottom();
 });
 
--- server.js
+++ server.js
@@ -1,49 +1,56 @@
-const express = require('express');
-const http = require('http');
-const cors=require('cors');
+const express = require("express");
+const http = require("http");
+const cors = require("cors");
 
 const app = express();
 const server = http.createServer(app);
 const io = require("socket.io")(server, {
-  cors:{
+  cors: {
     origin: "*",
-  }
+  },
 });
 
 app.use(express.static(__dirname));
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
 });
-
 
 const users = {};
 
-io.on('connection', (socket) => {
-  console.log('A user connected');
-  socket.on('join', (username) => {
-
-    users[socket.id] = { username, socketId: socket.id, time: (new Date().toLocaleString()) };
-
-    io.emit('update users', Object.values(users));
+io.on("connection", (socket) => {
+  console.log("New user connected");
+  socket.on("join", (username) => {
+    users[socket.id] = {
+      username,
+      socketId: socket.id,
+      time: new Date().toLocaleString(),
+    };
+    io.emit("user joined", users[socket.id]);
+    io.emit("update users", Object.values(users));
   });
 
-
-  socket.on('chat message', (msg) => {
+  socket.on("chat message", (message) => {
     const user = users[socket.id];
-    io.emit('chat message', { user, msg });
+    io.emit("chat message", {
+      user,
+      message,
+      hour: new Date().getHours(),
+      minute: new Date().getMinutes(),
+    });
   });
 
+  socket.on("disconnect", () => {
+    console.log("User disconnected");
 
-  socket.on('disconnect', () => {
-    console.log('User disconnected');
+    io.emit("user left", users[socket.id]);
 
     delete users[socket.id];
 
-    io.emit('update users', Object.values(users));
+    io.emit("update users", Object.values(users));
   });
 });
 
 server.listen(3000, () => {
-  console.log('Server listening on *:3000');
+  console.log("Server listening on http://localhost:3000");
 });
